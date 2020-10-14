0b6ff1e1f3d1b03d878187f8e93ea53eb4f81f23
Felix-kuang <71975005+Felix-kuang@users.noreply.github.com>
2020-10-14 09:09:08 +0700

    Add files via upload

--- lib/screens/Message_Screen.dart
+++ lib/screens/Message_Screen.dart
@@ -2,15 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:uts/config/pallete.dart';
 import 'package:uts/widgets/custom_appbar.dart';
 
-class MessageScreen extends StatefulWidget {
+class MessageScreen extends StatelessWidget {
   static const String id = "Message_Screen";
 
-  @override
-  _MessageScreenState createState() => _MessageScreenState();
-}
-
-class _MessageScreenState extends State<MessageScreen> {
-  String imgProfile =
+  final String imgProfile =
       "https://www.nicepng.com/png/full/87-874647_red-cross-hospital-logo-hospital-logo-red-cross.png";
 
   @override
