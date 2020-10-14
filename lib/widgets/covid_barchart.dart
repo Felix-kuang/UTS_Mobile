930c938112d36b9003f8cf00fd4e72183bfcb971
Felix-kuang <felixshi26@gmail.com>
2020-10-14 10:17:00 +0700

    Update covid_barchart.dart
    Perbaiki Overflow

--- lib/widgets/covid_barchart.dart
+++ lib/widgets/covid_barchart.dart
@@ -10,7 +10,7 @@ class CovidBarChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 350.0,
+      height: MediaQuery.of(context).size.height * 0.55,
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.only(
